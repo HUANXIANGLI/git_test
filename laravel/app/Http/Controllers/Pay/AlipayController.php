42c765fa8ffa3f8d3d778d8a4227b6c8de28e722
LIRUIXIANG <3077546454@qq.com>
2019-01-15 16:24:03 +0800

    回调

--- laravel/app/Http/Controllers/Pay/AlipayController.php
+++ laravel/app/Http/Controllers/Pay/AlipayController.php
@@ -13,26 +13,50 @@ class AlipayController extends Controller
     public $app_id;
     public $gate_way;
     public $notify_url;
+    public $return_url;
     public $rsaPrivateKeyFilePath = './key/priv.key';
+    public $aliPubKey = './key/ali_pub.key';
 
     public function __construct(){
         $this->app_id = env('PAT_ID');
         $this->gate_way = env('PAY_WAY');
-        $this->notify_url = env('PAY_URL');
+        $this->notify_url = env('NOTIFY_PAY_URL');
+        $this->return_url = env('RETURN_PAY_URL');
     }
 
-    public function test($o_id)
+    /**
+     * 订单支付
+     * @param $oid
+     */
+    public function pay($o_id)
     {
-        $order=OrderModel::where(['o_id'=>$o_id])->first();
 
+        //验证订单状态 是否已支付 是否是有效订单
+        $order_info = OrderModel::where(['o_id'=>$o_id])->first()->toArray();
+
+        //判断订单是否已被支付
+        if($order_info['is_pay']==1){
+            header('refresh:2,url=/orderList');
+            die("订单已支付，请勿重复支付");
+        }
+        //判断订单是否已被删除
+        if($order_info['is_delete']==1){
+            header('refresh:2,url=/orderList');
+            die("订单已被删除，无法支付");
+        }
+
+
+
+        //业务参数
         $bizcont = [
-            'subject'           => 'ancsd'. mt_rand(1111,9999).str_random(6),
+            'subject'           => 'Lening-Order: ' .$o_id,
             'out_trade_no'      => $o_id,
-            'total_amount'      => $order['o_amount']/100,
+            'total_amount'      => $order_info['o_amount'] / 100,
             'product_code'      => 'QUICK_WAP_WAY',
 
         ];
 
+        //公共参数
         $data = [
             'app_id'   => $this->app_id,
             'method'   => 'alipay.trade.wap.pay',
@@ -41,23 +65,23 @@ class AlipayController extends Controller
             'sign_type'   => 'RSA2',
             'timestamp'   => date('Y-m-d H:i:s'),
             'version'   => '1.0',
-            'notify_url'   => $this->notify_url,
+            'notify_url'   => $this->notify_url,        //异步通知地址
+            'return_url'   => $this->return_url,        // 同步通知地址
             'biz_content'   => json_encode($bizcont),
         ];
 
+        //签名
         $sign = $this->rsaSign($data);
         $data['sign'] = $sign;
         $param_str = '?';
         foreach($data as $k=>$v){
             $param_str .= $k.'='.urlencode($v) . '&';
         }
+
         $url = rtrim($param_str,'&');
         $url = $this->gate_way . $url;
-
-
         header("Location:".$url);
     }
-
 
     public function rsaSign($params) {
         return $this->sign($this->getSignContent($params));
@@ -78,7 +102,6 @@ class AlipayController extends Controller
         $sign = base64_encode($sign);
         return $sign;
     }
-
 
     public function getSignContent($params) {
         ksort($params);
@@ -113,7 +136,6 @@ class AlipayController extends Controller
         return false;
     }
 
-
     /**
      * 转换字符集编码
      * @param $data
@@ -131,6 +153,110 @@ class AlipayController extends Controller
 
 
         return $data;
+    }
+
+    /**
+     * 支付宝同步通知回调
+     */
+    public function aliReturn()
+    {
+        header('Refresh:2;url=/orderList');
+        echo "订单： ".$_GET['out_trade_no'] . ' 支付成功，正在跳转';
+//        echo '<pre>';print_r($_GET);echo '</pre>';die;
+//        //验签 支付宝的公钥
+//        if(!$this->verify($_GET)){
+//            die('簽名失敗');
+//        }
+//
+//        //验证交易状态
+////        if($_GET['']){
+////
+////        }
+////
+//
+//        //处理订单逻辑
+//        $this->dealOrder($_GET);
+    }
+
+    /**
+     * 支付宝异步通知
+     */
+    public function aliNotify()
+    {
+        $data = json_encode($_POST);
+        $log_str = '>>>> '.date('Y-m-d H:i:s') . $data . "<<<<\n\n";
+        //记录日志
+        file_put_contents('logs/alipay.log',$log_str,FILE_APPEND);
+        //验签
+        $res = $this->verify($_POST);
+
+        $log_str = '>>>> ' . date('Y-m-d H:i:s');
+        if($res === false){
+            //记录日志 验签失败
+            $log_str .= " Sign Failed!<<<<< \n\n";
+            file_put_contents('logs/alipay.log',$log_str,FILE_APPEND);
+        }else{
+            $log_str .= " Sign OK!<<<<< \n\n";
+            file_put_contents('logs/alipay.log',$log_str,FILE_APPEND);
+        }
+
+        //验证订单交易状态
+        if($_POST['trade_status']=='TRADE_SUCCESS'){
+            //更新订单状态
+            $oid = $_POST['out_trade_no'];     //商户订单号
+            $info = [
+                'is_pay'        => 1,       //支付状态  0未支付 1已支付
+                'pay_amount'    => $_POST['total_amount'] * 100,    //支付金额
+                'pay_time'      => strtotime($_POST['gmt_payment']), //支付时间
+                'plat_oid'      => $_POST['trade_no'],      //支付宝订单号
+                'plat'          => 1,      //平台编号 1支付宝 2微信
+                'status'        =>2
+            ];
+
+            OrderModel::where(['oid'=>$oid])->update($info);
+        }
+
+        //处理订单逻辑
+        $this->dealOrder($_POST);
+
+        echo 'success';
+    }
+
+    //验签
+    function verify($params) {
+        $sign = $params['sign'];
+        $params['sign_type'] = null;
+        $params['sign'] = null;
+
+        //读取公钥文件
+        $pubKey = file_get_contents($this->aliPubKey);
+        $pubKey = "-----BEGIN PUBLIC KEY-----\n" .
+            wordwrap($pubKey, 64, "\n", true) .
+            "\n-----END PUBLIC KEY-----";
+        //转换为openssl格式密钥
+
+        $res = openssl_get_publickey($pubKey);
+        ($res) or die('支付宝RSA公钥错误。请检查公钥文件格式是否正确');
+
+        //调用openssl内置方法验签，返回bool值
+
+        $result = (openssl_verify($this->getSignContent($params), base64_decode($sign), $res, OPENSSL_ALGO_SHA256)===1);
+        openssl_free_key($res);
+
+        return $result;
+    }
+
+    /**
+     * 处理订单逻辑 更新订单 支付状态 更新订单支付金额 支付时间
+     * @param $data
+     */
+    public function dealOrder($data)
+    {
+
+
+        //加积分
+
+        //减库存
     }
 }
 
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -15,64 +15,64 @@ Route::get('/', function () {
     return view('welcome');
 });
 //用户注册
-Route::any('userAdd', 'User\UserController@userAdd');
+Route::any('/userAdd', 'User\UserController@userAdd');
 
 //用户登录
-Route::any('loginAdd', 'User\UserController@loginAdd');
+Route::any('/loginAdd', 'User\UserController@loginAdd');
 
 //个人中心
-Route::any('center','User\UserController@center');
+Route::any('/center','User\UserController@center');
 
 //退出
-Route::any('loginQuit','User\UserController@loginQuit');
+Route::any('/loginQuit','User\UserController@loginQuit');
 
 
 
 //商品主页
-Route::any('goodsList','Goods\GoodsController@goodsList');
+Route::any('/goodsList','Goods\GoodsController@goodsList');
 
 //商品主页删除
-Route::any('goodsDel/{goods_id}','Goods\GoodsController@goodsDel')->middleware('check.login.token');
+Route::any('/goodsDel/{goods_id}','Goods\GoodsController@goodsDel')->middleware('check.login.token');
 
 //商品详情
-Route::any('goodsDetails/{goods_id}','Goods\GoodsController@goodsDetails')->middleware('check.login.token');
+Route::any('/goodsDetails/{goods_id}','Goods\GoodsController@goodsDetails')->middleware('check.login.token');
 
 
 
 
 //购物车展示
-Route::any('cartList','Cart\CartController@cartList')->middleware('check.login.token');
+Route::any('/cartList','Cart\CartController@cartList')->middleware('check.login.token');
 
 //购物车添加1
-Route::any('cartAdd/{goods_id}','Cart\CartController@cartAdd')->middleware('check.login.token');
+Route::any('/cartAdd/{goods_id}','Cart\CartController@cartAdd')->middleware('check.login.token');
 
 //购物车添加2
-Route::any('cartAdd2','Cart\CartController@cartAdd2')->middleware('check.login.token');
+Route::any('/cartAdd2','Cart\CartController@cartAdd2')->middleware('check.login.token');
 
 //购物车删除1
-Route::any('cartDel/{goods_id}','Cart\CartController@cartDel')->middleware('check.login.token');
+Route::any('/cartDel/{goods_id}','Cart\CartController@cartDel')->middleware('check.login.token');
 
 //购物车删除2
-Route::any('cartDel2/{c_id}','Cart\CartController@cartDel2')->middleware('check.login.token');
+Route::any('/cartDel2/{c_id}','Cart\CartController@cartDel2')->middleware('check.login.token');
 
 
 
 
 //提交订单
-Route::any('orderAdd','Order\OrderController@orderAdd')->middleware('check.login.token');
+Route::any('/orderAdd','Order\OrderController@orderAdd')->middleware('check.login.token');
 
 //订单展示
-Route::any('orderList','Order\OrderController@orderList')->middleware('check.login.token');
+Route::any('/orderList','Order\OrderController@orderList')->middleware('check.login.token');
 
 //删除订单
-Route::any('orderDel/{o_id}','Order\OrderController@orderDel')->middleware('check.login.token');
+Route::any('/orderDel/{o_id}','Order\OrderController@orderDel')->middleware('check.login.token');
 
 //支付订单
-Route::any('orderPay/{o_id}','Order\OrderController@orderPay')->middleware('check.login.token');
+Route::any('/orderPay/{o_id}','Order\OrderController@orderPay')->middleware('check.login.token');
 
 //跳转网址
-Route::any('Pay','Order\OrderController@pay');
-Route::get('/payTest/{o_id}','Pay\AlipayController@test');         //测试
-Route::post('payNotify','Pay\AlipayController@notify');        //支付宝支付 通知回调
+Route::any('/Pay/{o_id}','Pay\AlipayController@pay');
+Route::post('/aliNotify','Pay\AlipayController@aliNotify');        //支付宝支付 异步通知回调
+Route::any('/aliReturn','Pay\AlipayController@aliReturn');        //支付宝支付 同步通知回调
 
 
