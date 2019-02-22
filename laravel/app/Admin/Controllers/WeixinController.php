88b4815874fead4e6643d78ea94f5b11287f2d13
LIRUIXIANG <3077546454@qq.com>
2019-02-22 17:07:33 +0800

    wx

--- laravel/app/Admin/Controllers/WeixinController.php
+++ laravel/app/Admin/Controllers/WeixinController.php
@@ -17,6 +17,8 @@ class WeixinController extends Controller
 {
     use HasResourceActions;
 
+    protected $redis_weixin_access_token = 'str:weixin_access_token';     //微信 access_token
+
     /**
      * Index interface.
      *
