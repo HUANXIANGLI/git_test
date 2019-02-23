53b670b4acbbb729a73d990bcfa5c36910f70380
LIRUIXIANG <3077546454@qq.com>
2019-02-23 11:41:25 +0800

    wx

--- laravel/app/Admin/routes.php
+++ laravel/app/Admin/routes.php
@@ -18,7 +18,7 @@ Route::group([
     $router->resource('/wxmedia',WeixinMediaController::class);
     $router->resource('/material',WeixinMaterialController::class);
 
-    $router->get('/weixin/information/{id}','WeixinController@information');      //
+    $router->get('/information/{id}','WeixinController@information');      //
 
     $router->get('/weixin/sendmsg','WeixinController@sendMsgView');      //
     $router->post('/weixin/sendmsg','WeixinController@sendMsg');
