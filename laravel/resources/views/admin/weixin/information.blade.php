e34ecedaded5650eb8d7bd44d61a1d12206efbb1
LIRUIXIANG <3077546454@qq.com>
2019-02-23 10:32:55 +0800

    wx

--- laravel/resources/views/admin/weixin/information.blade.php
+++ laravel/resources/views/admin/weixin/information.blade.php
@@ -26,7 +26,7 @@
                     <div><img style="width:50px;height:50px;" src="{{env('IMG_URL')}}form_test/0mnJruzhTpc1B5d.jpg" alt=""></div>
                     <div class="content">
                         <p class="author">Nice奶思</p>
-                        <div class="msg" style="background-color: fuchsia">
+                        <div class="msg" >
                             么么哒，马总发个红包呗！
                         </div>
                     </div>
@@ -45,7 +45,7 @@
                     <div><img style="width:50px;height:50px;" src="{{env('IMG_URL')}}form_test/0mnJruzhTpc1B5d.jpg" alt=""></div>
                     <div class="content">
                         <p class="author">Nice奶思</p>
-                        <div class="msg" style="background-color: fuchsia">
+                        <div class="msg" >
                             么么哒，马总发个红包呗！
                         </div>
                     </div>
