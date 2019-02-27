2dc03c65c7303d74b655a77290794cd4ef3ddaa8
LIRUIXIANG <3077546454@qq.com>
2019-02-27 15:00:13 +0800

    二维码

--- laravel/app/Http/Controllers/Weixin/PayController.php
+++ laravel/app/Http/Controllers/Weixin/PayController.php
@@ -62,8 +62,7 @@ class PayController extends Controller
         include_once('phpqrcode/phpqrcode.php');
         $url=$data->code_url;
 
-        $data=rand(11111,99999) . rand(2222,9999);
-        $file_name='picture/'.$data.'.png';
+        $file_name='picture/'.$res['o_name'].'.png';
         \QRcode::png($url,$file_name,'H','5','1');
         //echo '<img src="'.$file_name.'">';die;
         $data=[
