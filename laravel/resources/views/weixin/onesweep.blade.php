6fcae5a986c3d06290355575e74a94de4cb936af
LIRUIXIANG <3077546454@qq.com>
2019-02-28 20:27:26 +0800

    wx

--- laravel/resources/views/weixin/onesweep.blade.php
+++ laravel/resources/views/weixin/onesweep.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <h2>微信登录</h2>
         <h3>
-            <a href="https://open.weixin.qq.com/connect/qrconnect?appid=wxe24f70961302b5a5&amp;redirect_uri=http%3a%2f%2fmall.77sc.com.cn%2fweixin.php%3fr1%3dhttp%3a%2f%2fig.anjingdehua.cn%2fweixin%2fgetcode&amp;response_type=code&amp;scope=snsapi_login&amp;state=STATE#wechat_redirect">Login</a>
+            <a href="https://open.weixin.qq.com/connect/qrconnect?appid=wxe24f70961302b5a5&amp;redirect_uri=http%3A%2F%2Fmall.77sc.com.cn%2Fweixin.php%3Fr1%3Dhttp%3A%2F%2Fig.anjingdehua.cn%2FcodeAdd&amp;response_type=code&amp;scope=snsapi_login&amp;state=STATE#wechat_redirect">Login</a>
         </h3>
     </div>
 @endsection
